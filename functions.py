74ad83ae9810d9b6d244670637d57557b385f647
Scottie Schneider <scott@laclave.co>
2017-09-14 17:36:34 -0500

    Exceptions and functions added

---
+++ functions.py
@@ -0,0 +1,15 @@
+def hows_the_parrot():
+    print("He's pining for the fjords!")
+
+hows_the_parrot()
+
+def lumberjack(name, pronoun):
+    print("{} sleeps all night and {} works all day!".format(name, pronoun))
+
+lumberjack("dog", "he's")
+
+def average(num1, num2):
+    avg = (num1+num2) / 2
+    print avg
+    return avg
+average(2,8)
